68e1d0e94b22642bfab4a7fce7e44a4f04bc1fde
lucetrez <149235148+lucetrez@users.noreply.github.com>
2024-07-20 17:59:31 +0900

    Delete prebuild.js

--- prebuild.js
+++
@@ -1,8 +0,0 @@
-import { sync } from 'replace-in-file';
-const result = sync({
-  files: 'build/index.html',
-  from: /<%= REACT_APP_GLNK_USERNAME %>/g,
-  to: process.env.REACT_APP_GLNK_USERNAME || 'default',
-});
-
-console.log('Replacement results:', result);
